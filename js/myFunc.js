9fe52d9ad53825b28c7fdca6b06146ee9c462d05
Shun-Ichiro Kaneshiro <pinkroot@gmail.com>
2012-06-30 13:31:09 +0900

    formの基本構造作った

---
+++ js/myFunc.js
@@ -0,0 +1,13 @@
+function submitFunc() {
+  var input_ans = $("#input_text").val();
+	
+	$("#test_str").html(input_ans);
+
+}
+
+function push_enter(){
+	if( window.event.keyCode == 13 ){
+		//document.form1.submit();
+		submitFunc();
+	}	
+}
